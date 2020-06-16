5944f4fc22e2248a19ab0a0dae6cb54065a812e3
CB <cbshrestha24@gmail.com>
2020-06-17 03:44:16 +1000

    following features added

---
+++ routes/user.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requirelog')
+const requirelog = require('../middleware/requirelog')
+const Post = mongoose.model('Post')
+const User = mongoose.model("User")
+
+router.get('/user/:id',requirelog,(req,res)=>{
+    User.findOne({_id:req.params.id})
+    .select("-password")
+    .then(user=>{
+        Post.find({postedBy:req.params.id})
+        .populate("postedBy","_id name")
+        .exec((error, post)=>{
+            if(error){
+                return res.status(422).json({error:error})
+            }
+            res.json({user, post})
+
+        })
+    })
+    .catch(error=>{
+        return res.status(404).json({error:"User not found"})
+    })
+})
+
+
+
+router.put('/follow',requirelog,(req,res)=>{
+    User.findByIdAndUpdate(req.body.followId,{
+        $push:{followers:req.user._id}
+    },{
+        new:true
+    },(error,result)=>{
+        if(error){
+            return res.status(422).json({error:error})
+        }
+
+        User.findByIdAndUpdate(req.user._id,{
+            
+            $push:{following:req.body.followId}
+        },{
+            new:true
+        })
+        .select("-password")
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(error=>{
+            return res.status(422).json({error:error})
+        })
+
+    })
+})
+
+
+
+router.put('/unfollow',requirelog,(req,res)=>{
+    User.findByIdAndUpdate(req.body.unfollowId,{
+        $pull:{followers:req.user._id}
+    },{
+        new:true
+    },(error,result)=>{
+        if(error){
+            return res.status(422).json({error:error})
+        }
+
+        User.findByIdAndUpdate(req.user._id,{
+            $pull:{following:req.body.unfollowId}
+        },{
+            new:true
+        })
+        .select("-password")
+        .then(result=>{
+            res.json(result)
+        })
+        .catch(error=>{
+            return res.status(422).json({error:error})
+        })
+
+    })
+})
+
+
+module.exports = router
