a08459ab044b198bc947ae61823c9b02c4911a5f
CB <cbshrestha24@gmail.com>
2020-06-18 02:27:35 +1000

    updates one

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,18 @@ const bcrypt = require('bcryptjs')
 const jwt =require('jsonwebtoken')
 const {JWT_SECRET} = require('../config/keys')
 const requirelog = require('../middleware/requirelog')
+const nodemailer = require('nodemailer')
+const sendgridtransport = require('nodemailer-sendgrid-transport')
 
 
-
-router.get('/protected',requirelog,(req,res)=>{
-    res.send('Hello user')
-})
+const transporter  = nodemailer.createTransport(sendgridtransport({
+    auth:{
+        api_key:"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"
+    }
+}))
 
 router.post('/signup',(req,res)=>{
     const {name, email, password,pic} = req.body
-    console.log(req.body)
     if(!email || !password || !name){
       return res.status(422).json({error:'Please add all the fields'})
     }
@@ -39,6 +41,12 @@ router.post('/signup',(req,res)=>{
     
             user.save()
             .then(user=>{
+                transporter.sendMail({
+                    to:user.email,
+                    from:"no-reply@pshare.com",
+                    subject:"Signup success",
+                    html:'<h1>Welcome to pshare application</h1>'
+                })
                 res.json({
                     message:'Saved'
                 })
