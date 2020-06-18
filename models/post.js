1890acd5daeffa54c8cda6031347f4426140bfd3
CB <cbshrestha24@gmail.com>
2020-06-19 03:25:50 +1000

    updated link

--- models/post.js
+++ models/post.js
@@ -29,6 +29,8 @@ const postSchema = new mongoose.Schema({
         type:ObjectId,
         ref:'User'
     }
+},{
+    timestamps:true
 })
 
 mongoose.model('Post', postSchema)
--- routes/post.js
+++ routes/post.js
@@ -11,6 +11,7 @@ router.get('/allpost',requirelog,(req,res)=>{
     Post.find()
     .populate("postedBy","_id name")
     .populate("comments.postedBy","_id name")
+    .sort('-createdAt')
     .then(posts=>{
         res.json({
             posts
@@ -26,6 +27,7 @@ router.get('/getsubpost',requirelog,(req,res)=>{
     Post.find({postedBy:{$in:req.user.following}})
     .populate("postedBy","_id name")
     .populate("comments.postedBy","_id name")
+    .sort('-createdAt')
     .then(posts=>{
         res.json({
             posts
